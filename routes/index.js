d92a4ccab25027128d7ebd64ffc513ec06588e14
Ivan Chyr - Codete <ivan.chyr@codete.com>
2020-04-01 11:28:09 +0200

    added debugger command

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log("In here");
+  console.log("pravda");
   res.render('index', { title: 'Express' });
 });
 
